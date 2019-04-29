e52f63103d1be05af267e34fa459d3435eea7579
Balaji, Sathish <sbalaji@in.imshealth.com>
2019-04-29 13:09:25 +0530

    sd

--- einstein/BatchService.js
+++ einstein/BatchService.js
@@ -41,6 +41,8 @@ class BatchService {
             let ag = new Agendax();
             await ag.connection();
             console.log('-*-*-*-*-: '+result);
+            console.log('-*-*-*-*-: '+Object.keys(result));
+            console.log('-*-*-*-*-: '+Object.keys(result));
             ag.setData([result]);
             ag.loadJob('PredictJob');
             ag.startx(); // runs job in background process
